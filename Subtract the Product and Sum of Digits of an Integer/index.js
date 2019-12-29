d2a0c8e1bf6e0486bace6852e03d1d6ae7dd6587
yehao <yehao@kuaishou.com>
2019-12-29 14:41:00 +0800

    Subtract the Product and Sum of Digits of an Integer

---
+++ Subtract the Product and Sum of Digits of an Integer/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function (n) {
+  var product = 1;
+  var sum = 0;
+
+  while (n > 0) {
+    var digit = n % 10;
+    product *= digit;
+    sum += digit
+    n = parseInt(n / 10);
+  }
+
+  return product - sum;
+};
